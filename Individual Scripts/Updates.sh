48f6053aff7241d72904226877f9477dca4fbacb
Th3-0 <74868302+Th3-0@users.noreply.github.com>
2023-09-25 08:26:33 -0500

    Forgot to remove old stuff

--- Individual Scripts/Updates.sh
+++ Individual Scripts/Updates.sh
@@ -24,59 +24,6 @@ done
 
 
 
-
-
-
-
-
-#focal
-if grep -iq "deb http://us.archive.ubuntu.com/ubuntu/ focal" /etc/apt/sources.list
-then
-    sudo sed -i '/deb http:\/\/us.archive.ubuntu.com\/ubuntu\/ focal/c\deb http:\/\/archive.ubuntu.com\/ubuntu\/ focal main restricted universe' /etc/apt/sources.list
-else
-    echo "deb http://us.archive.ubuntu.com/ubuntu/ focal main restricted universe" >> /etc/apt/sources.list
-fi
-
-#focal-updates
-if grep -iq "deb http://us.archive.ubuntu.com/ubuntu/ focal-updates" /etc/apt/sources.list
-then
-    sudo sed -i '/deb http:\/\/us.archive.ubuntu.com\/ubuntu\/ focal-updates/c\deb http:\/\/archive.ubuntu.com\/ubuntu\/ focal-updates main restricted universe' /etc/apt/sources.list
-else
-    echo "deb http://us.archive.ubuntu.com/ubuntu/ focal-updates main restricted universe" >> /etc/apt/sources.list
-fi
-
-#focal-security
-if grep -iq "deb http://us.archive.ubuntu.com/ubuntu focal-security" /etc/apt/sources.list
-then
-    sudo sed -i '/deb http:\/\/us.archive.ubuntu.com\/ubuntu focal-security/c\deb http:\/\/archive.ubuntu.com\/ubuntu focal-security main restricted universe' /etc/apt/sources.list
-else
-    echo "deb http://us.archive.ubuntu.com/ubuntu focal-security main restricted universe" >> /etc/apt/sources.list
-fi
-
-#focal-backports
-if grep -iq "deb http://us.archive.ubuntu.com/ubuntu/ focal-backports" /etc/apt/sources.list
-then
-    sudo sed -i '/deb http:\/\/us.archive.ubuntu.com\/ubuntu\/ focal-backports/c\deb http:\/\/archive.ubuntu.com\/ubuntu\/ focal-backports main restricted universe' /etc/apt/sources.list
-else
-    echo "deb http://us.archive.ubuntu.com/ubuntu/ focal-backports main restricted universe" >> /etc/apt/sources.list
-fi
-
-
-#canonical Partner
-if grep -iq "deb http://archive.canonical.com/ubuntu focal" /etc/apt/sources.list
-then
-    sudo sed -i '/deb http:\/\/archive.canonical.com\/ubuntu focal/c\deb http:\/\/archive.canonical.com\/ubuntu focal partner' /etc/apt/sources.list
-else
-    echo "deb http://archive.canonical.com/ubuntu focal partner" >> /etc/apt/sources.list
-fi
-
-if grep -iq "deb-src http://archive.canonical.com/ubuntu focal" /etc/apt/sources.list
-then
-    sudo sed -i '/deb-src http:\/\/archive.canonical.com\/ubuntu focal/c\deb-src http:\/\/archive.canonical.com\/ubuntu focal partner' /etc/apt/sources.list
-else
-    echo "deb-src http://archive.canonical.com/ubuntu focal partner" >> /etc/apt/sources.list
-fi
-
 sudo sed -i '/Prompt/c\Prompt=lts' /etc/update-manager/release-upgrades
 sudo sed -i '/APT::Periodic::Update-Package-Lists/c\APT::Periodic::Update-Package-Lists "1";' /etc/apt/apt.conf.d/*auto-upgrades /etc/apt/apt.conf.d/*periodic
 sudo sed -i '/APT::Periodic::Download-Upgradeable-Packages/c\APT::Periodic::Download-Upgradeable-Packages "1";' /etc/apt/apt.conf.d/*auto-upgrades /etc/apt/apt.conf.d/*periodic
