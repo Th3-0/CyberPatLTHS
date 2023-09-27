656a32e9fac179e49bb205c1a15f58926b5ce9ce
christianjanev <christianjanev7@gmail.com>
2023-09-26 21:00:54 -0500

    Add Firewall Script

---
+++ Individual Scripts/Firewall.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+# Firewall
+
+if ! dpkg -l | grep -q 'ufw'; then
+    echo "ufw was not found...installing"
+    sudo apt install ufw -yq
+fi
+
+if sudo ufw status | grep -q 'inactive'; then
+    echo "ufw is inactive...enabling"
+    sudo ufw enable
+fi
+
+echo "turning on logging"
+sudo ufw logging on
+
+echo "allowing ports"
+# sudo ufw allow `echo $@ | sed 's/ /,/g'`
+# work on this on thursday
+echo "use sudo ufw allow <port> to allow a port, i'll add this later"
+
+if ! grep -q ipv6.disable=1 /etc/default/grub; then
+    echo "disabling ipv6"
+    sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"[^ ]*/& ipv6.disable=1/" /etc/default/grub
+    sudo update-grub
+fi
+
+echo "updating sysctl config"
+
+if grep -q '#net.ipv4.conf.default.rp_filter.*=.*1' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv4.conf.default.rp_filter.*=.*1/s/^#//g' /etc/sysctl.conf
+elif ! grep -q 'net.ipv4.conf.default.rp_filter.*=.*1' /etc/sysctl.conf; then
+    sudo echo "net.ipv4.conf.default.rp_filter=1" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+if grep -q '#net.ipv4.conf.all.rp_filter.*=.*1' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv4.conf.all.rp_filter.*=.*1/s/^#//g' /etc/sysctl.conf
+elif ! grep -q 'net.ipv4.conf.all.rp_filter.*=.*1' /etc/sysctl.conf; then
+    sudo echo "net.ipv4.conf.all.rp_filter=1" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+if grep -q '#net.ipv4.conf.all.accept_redirects.*=.*0' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv4.conf.all.accept_redirects.*=.*0/s/^#//g' /etc/sysctl.conf
+elif ! grep -q 'net.ipv4.conf.all.accept_redirects.*=.*0' /etc/sysctl.conf; then
+    echo "net.ipv4.conf.all.accept_redirects=0" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+if grep -q '#net.ipv4.conf.all.send_redirects.*=.*0' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv4.conf.all.send_redirects.*=.*0/s/^#//g' /etc/sysctl.conf
+elif ! grep -q 'net.ipv4.conf.all.send_redirects.*=.*0' /etc/sysctl.conf; then
+    sudo echo "net.ipv4.conf.all.send_redirects=0" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+if grep -q '#net.ipv4.conf.all.accept_source_route.*=.*0' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv4.conf.all.accept_source_route.*=.*0/s/^#//g' /etc/sysctl.conf
+elif ! grep -q 'net.ipv4.conf.all.accept_source_route.*=.*0' /etc/sysctl.conf; then
+    sudo echo "net.ipv4.conf.all.accept_source_route=0" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+if grep -q '#net.ipv4.conf.all.log_martians.*=.*1' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv4.conf.all.log_martians.*=.*1/s/^#//g' /etc/sysctl.conf
+elif ! grep -q 'net.ipv4.conf.all.log_martians.*=.*1' /etc/sysctl.conf; then
+    sudo echo "net.ipv4.conf.all.log_martians=1" | sudo tee -a /etc/sysctl.conf  > /dev/null
+fi
+
+if grep -q -x '.*net.ipv4.ip_forward.*=.*0' /etc/sysctl.conf; then
+    sudo sed -i '/.*net.ipv4.ip_forward.*=.*0/s/^/#/g' /etc/sysctl.conf
+elif ! grep -q '.*net.ipv4.ip_forward.*=.*0' /etc/sysctl.conf; then
+    sudo echo "net.ipv4.ip_forward=0" | sudo tee -a /etc/sysctl.conf  > /dev/null
+fi
+
+if grep -q '#net.ipv4.tcp_syncookies.*=.*1' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv4.tcp_syncookies.*=.*1/s/^#//g' /etc/sysctl.conf
+elif ! grep -q 'net.ipv4.tcp_syncookies.*=.*1' /etc/sysctl.conf; then
+    sudo echo "net.ipv4.tcp_syncookies=1" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+if grep -q 'net.ipv6.conf.all.disable_ipv6.*=.*1' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv6.conf.all.disable_ipv6.*=.*1/s/^#//g' /etc/sysctl.conf
+elif ! grep -q 'net.ipv6.conf.all.disable_ipv6.*=.*1' /etc/sysctl.conf; then
+    sudo echo "net.ipv6.conf.all.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+sudo sysctl -p > /dev/null
+
+
+#do this on thursday
+echo "Check /etc/hosts for suspicious lines"
