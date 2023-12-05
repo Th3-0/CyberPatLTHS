68f9b1277471c073e77f7d5fb3bb7591baab2797
Th3-0 <74868302+Th3-0@users.noreply.github.com>
2023-12-04 19:17:33 -0600

    Added Ref Files

--- Tools/PackageCompare/PackageCompare.sh
+++ Tools/PackageCompare/PackageCompare.sh
@@ -4,10 +4,9 @@ echo "        Select Database to Compare"
 echo "================================================"
 echo "         1. Base Install Packages"
 echo "         2. Full Install Packages"
-echo "         3. Security Packages"
-echo "         4. Focal Updates"
-echo "         5. Focal All"
-echo "         6. Custom"
+echo "         3. Focal Updates"
+echo "         4. Focal All"
+echo "         5. Custom"
 A=true
 echo "=============================================================" >> Package_Differences
 dpkg-query -W -f='${binary:Package}\n' > PackageList
@@ -15,9 +14,8 @@ read -p "" input
     case $input in
         1) Package="BaseRef";;
         2) Package="FullRef";;
-        3) Package="SecRef";;
-        4) Package="UpdRef";;
-        5) Package="FAllRef";;
+        3) Package="UpdRef";;
+        4) Package="FAllRef";;
         5) Custom;;
     esac
 
