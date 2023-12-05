87482511c64dd5edfd4902c71cbef907a1600727
Th3-0 <H31i05.h3110@protonmail.com>
2023-12-04 20:16:44 -0600

    Added further filtering for package compare

--- Individual Scripts/PackageCompare.sh
+++ Individual Scripts/PackageCompare.sh
@@ -20,16 +20,29 @@ read -p "" input
         5) Package="FAllRef";;
         5) Custom;;
     esac
-
+read -p "Filter out Packages not on system(type y or n): " filter
 Custom() {
     read -p "Please input Reference File Name: "$custom
-    diff -u PackageList $custom | sed -n '1,2d;/^[-+]/p' >> Package_Differences
+    diff -u PackageList $custom | sed -n '1,2d;/^[-+]/p' > temp
+    if [ $filter ="n" ]
+    then
+        sed '/^+/ d' < temp >> Package_Differences
+    else
+        echo < temp >> Package Differences
+    fi
     A=false
 }
 
 if [ $A = true ] 
 then
-    diff -u PackageList $Package | sed -n '1,2d;/^[-+]/p' >> Package_Differences
+    diff -u PackageList $Package | sed -n '1,2d;/^[-+]/p' > temp
     echo "Differences are stored in Package_Differences"
+    if [ $filter ="n"]
+    then
+        sed '/^+/ d' < temp >> Package_Differences
+    else
+        echo < temp >> Package Differences
+    fi
 fi
 rm PackageList
+rm temp
